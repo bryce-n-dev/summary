e321dc5374a17d23086a0af80cfc81651f72cbcb
Bryce <bryce@Bryces-MacBook-Air.local>
2022-06-05 15:11:56 -0600

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>summary</h1>
+      <p>good morning bryce, here's what we've found for you today.</p>
+      <h2>weather</h2>
+      <h2>email</h2>
+      <h2>youtube</h2>
+    </div>
+  );
+}
+
+export default App;
